97283dfdb27503529eb2c949415f2aa6031fb81d
Will <wrightw404@gmail.com>
2022-11-16 18:24:51 -0800

    added final functionality that allows items to be saved in local storage

--- script.js
+++ script.js
@@ -25,5 +25,21 @@ $(document).ready(function() {
                 $(this).addClass('future');
             }
         })
-    }
-})
+    }   
+
+    updateHour();
+
+    var interval = setInterval(updateHour, 15000);
+
+    $('9hour .description').val(localStorage.getItem('9hour'));
+    $('10hour .description').val(localStorage.getItem('10hour'));
+    $('11hour .description').val(localStorage.getItem('11hour'));
+    $('12hour .description').val(localStorage.getItem('12hour'));
+    $('13hour .description').val(localStorage.getItem('13hour'));
+    $('14hour .description').val(localStorage.getItem('14hour'));
+    $('15hour .description').val(localStorage.getItem('15hour'));
+    $('16hour .description').val(localStorage.getItem('16hour'));
+    $('17hour .description').val(localStorage.getItem('17hour'));
+
+    $('#currentDay').text(moment().format('dddd MMMM Do'));
+});
